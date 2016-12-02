77fe5dae49dc2338bba34e0b1af5085b64ac78d9
juliannunez <julian.nunez@udea.edu.co>
2016-12-01 19:28:37 -0500

    Este es el proyecto aun sin backend

---
+++ api/v1/authentication.php
@@ -0,0 +1,83 @@
+<?php 
+$app->get('/session', function() {
+    $db = new DbHandler();
+    $session = $db->getSession();
+    $response["uid"] = $session['uid'];
+    $response["email"] = $session['email'];
+    $response["name"] = $session['name'];
+    echoResponse(200, $session);
+});
+
+$app->post('/login', function() use ($app) {
+    require_once 'passwordHash.php';
+    $r = json_decode($app->request->getBody());
+    verifyRequiredParams(array('email', 'password'),$r->customer);
+    $response = array();
+    $db = new DbHandler();
+    $password = $r->customer->password;
+    $email = $r->customer->email;
+    $user = $db->getOneRecord("select uid,name,password,email,created from customers_auth where phone='$email' or email='$email'");
+    if ($user != NULL) {
+        if(passwordHash::check_password($user['password'],$password)){
+        $response['status'] = "success";
+        $response['message'] = 'Logged in successfully.';
+        $response['name'] = $user['name'];
+        $response['uid'] = $user['uid'];
+        $response['email'] = $user['email'];
+        $response['createdAt'] = $user['created'];
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+        $_SESSION['uid'] = $user['uid'];
+        $_SESSION['email'] = $email;
+        $_SESSION['name'] = $user['name'];
+        } else {
+            $response['status'] = "error";
+            $response['message'] = 'Login failed. Incorrect credentials';
+        }
+    }else {
+            $response['status'] = "error";
+            $response['message'] = 'No such user is registered';
+        }
+    echoResponse(200, $response);
+});
+$app->post('/signUp', function() use ($app) {
+    $response = array();
+    $r = json_decode($app->request->getBody());
+    $db = new DbHandler();
+    $idType = $r->customer->idType;
+    $id = $r->customer->id;
+    $name = $r->customer->name;
+    $email = $r->customer->linkCv;
+    $phone = $r->customer->phone;
+    $email = $r->customer->email;
+    $address = $r->customer->address;
+    $isUserExists = $db->getOneRecord("select 1 from profesor where nro_doc='$id'");
+    if(!$isUserExists){
+        $tabble_name = "profesor";
+        $column_names = array('tipo_doc', 'nro_doc', 'nombre', 'link_cv', 'celular', 'direccion', 'email');
+        $result = $db->insertIntoTable($r->customer, $column_names, $tabble_name);
+        if ($result != NULL) {
+            $response["status"] = "success";
+            $response["message"] = "User account created successfully";
+            $response["uid"] = $result;
+            echoResponse(200, $response);
+        } else {
+            $response["status"] = "error";
+            $response["message"] = "Failed to create customer. Please try again";
+            echoResponse(201, $response);
+        }            
+    }else{
+        $response["status"] = "error";
+        $response["message"] = "An user with the provided phone or email exists!";
+        echoResponse(201, $response);
+    }
+});
+$app->get('/logout', function() {
+    $db = new DbHandler();
+    $session = $db->destroySession();
+    $response["status"] = "info";
+    $response["message"] = "Logged out successfully";
+    echoResponse(200, $response);
+});
+?>
